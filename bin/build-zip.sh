99ddc5083d8626fb6102b0824d323ada179c18ab
Matthew Harris <rtpharry@hotmail.com>
2025-03-15 13:10:29 +0000

    chore(build): new build pipeline

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 PLUGIN_DIR=$(pwd)
-PLUGINSLUG="runthings-wc-coupons-role-restrict"
+PLUGINSLUG=$(basename "$PLUGIN_DIR")
 BUILD_DIR="${PLUGIN_DIR}/build"
 DISTIGNORE="${PLUGIN_DIR}/.distignore"
-MAKEPOT_SCRIPT="${PLUGIN_DIR}/bin/makepot.sh"
+LANG_DIR="languages"
+POT_FILE="$LANG_DIR/$PLUGINSLUG.pot"
+
+# That's all, stop editing! Happy building.
 
 # Function to check for required tools
 check_tool() {
@@ -18,6 +21,7 @@ check_tool() {
 check_tool rsync
 check_tool zip
 check_tool mktemp
+check_tool wp
 
 # Check if the script is being run from the root directory of the plugin
 if [[ ! -f "${PLUGIN_DIR}/${PLUGINSLUG}.php" ]]; then
@@ -26,10 +30,10 @@ if [[ ! -f "${PLUGIN_DIR}/${PLUGINSLUG}.php" ]]; then
   exit 1
 fi
 
-# Run makepot.sh to generate/update translation files
-echo "Running makepot.sh to generate/update translation files..."
-if ! ${MAKEPOT_SCRIPT}; then
-  echo "Error: makepot.sh failed."
+# Generate the .pot file
+echo "Generating .pot file..."
+if ! wp i18n make-pot . $POT_FILE --domain=$PLUGINSLUG; then
+  echo "Error: Failed to generate .pot file."
   exit 1
 fi
 
--- bin/makepot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wp i18n make-pot . languages/runthings-wc-coupons-role-restrict.pot --domain=runthings-wc-coupons-role-restrict
